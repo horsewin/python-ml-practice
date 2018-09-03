c37ed753a42101afa45503ad13b584ef49cf4626
AtsushiUMAKATSU <mamamatz@gmail.com>
2018-09-03 18:56:06 +0900

    chapter 6 pipeline

--- chapter6_pipeline.py
+++ chapter6_pipeline.py
@@ -1,9 +1,10 @@
+from sklearn.linear_model import Ridge
 from sklearn.svm import SVC
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_breast_cancer, load_boston
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures, StandardScaler
 from sklearn.pipeline import Pipeline, make_pipeline
-
+import matplotlib.pyplot as plt
 
 def bad_usecase():
     cancer = load_breast_cancer()
@@ -70,8 +71,36 @@ def pipeline_shorthand():
     print("Best parameters:{}".format(grid.best_params_, ))
 
 
-#
+def pipeline_actual_data():
+    boston = load_boston()
+    X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, random_state=0)
+    pipe = make_pipeline(
+        StandardScaler(),
+        PolynomialFeatures(),
+        Ridge()
+    )
+
+    param_grid = {
+        "polynomialfeatures__degree": [1, 2, 3],
+        "ridge__alpha": [0.001, 0.01, 0.1, 1, 10, 100]
+    }
+
+    grid = GridSearchCV(pipe, param_grid=param_grid, cv=5, n_jobs=-1)
+    grid.fit(X_train, y_train)
+
+    plt.matshow(
+        grid.cv_results_["mean_test_score"].reshape(3, -1), vmin=0, cmap="viridis"
+    )
+
+    plt.xlabel("ridge__alpha")
+    plt.ylabel("polynomialfeatures__degree")
+    plt.xticks(range(len(param_grid["ridge__alpha"])), param_grid["ridge__alpha"])
+    plt.yticks(range(len(param_grid["polynomialfeatures__degree"])), param_grid["polynomialfeatures__degree"])
+    plt.colorbar()
+    plt.show()
+
 # bad_usecase()
 # pipeline_usecase()
 # pipeline_grid_search()
-pipeline_shorthand()
+# pipeline_shorthand()
+pipeline_actual_data()
